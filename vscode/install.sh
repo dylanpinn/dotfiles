4057dbb908149510623c9bb19fda46fe3a309bf5
Dylan Pinn <dylan@dylanpinn.com>
2018-01-15 21:19:00 +1100

    [vscode] update to use rcm

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,26 +1,6 @@
-#!/bin/sh
-if test "$(which code)"; then
-  if [ "$(uname -s)" = "Darwin" ]; then
-    VSCODE_HOME="$HOME/Library/Application Support/Code"
-  else
-    VSCODE_HOME="$HOME/.config/Code"
-  fi
+#!/usr/bin/env bash
 
-  ln -sf "$DOTFILES/vscode/settings.json" "$VSCODE_HOME/User/settings.json"
-  ln -sf "$DOTFILES/vscode/keybindings.json" "$VSCODE_HOME/User/keybindings.json"
-
-  # from `code --list-extensions`
-  modules="
-    EditorConfig.EditorConfig
-    PeterJausovec.vscode-docker
-    WakaTime.vscode-wakatime
-    arcticicestudio.nord-visual-studio-code
-    rebornix.Ruby
-    timonwong.shellcheck
-    shinnn.stylelint
-    minhthai.vscode-todo-parser
-"
-  for module in $modules; do
-    code --install-extension "$module" || true
-  done
+# Symlink to more default dir if macOS
+if [[ "$OSTYPE" == darwin* ]]; then
+  ln -sf $HOME/.config/Code/User/*.json "$HOME/Library/Application Support/Code/User"
 fi
