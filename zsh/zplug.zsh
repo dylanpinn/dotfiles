48c3ab65222a9c044475916e8985ad30d2252f05
Dylan Pinn <dylan@dylanpinn.com>
2018-01-15 21:54:00 +1100

    Add more comments

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -9,6 +9,7 @@ if [ -z ${RELOAD} ]; then
   zplug "zplug/zplug", hook-build:"zplug --self-manage"
   zplug "zsh-users/zsh-syntax-highlighting", defer:2
   zplug "zsh-users/zsh-autosuggestions"
+  # Prompt
   zplug "mafredri/zsh-async", from:github
   zplug "sindresorhus/pure", use:pure.zsh, from:github, as:theme
 
