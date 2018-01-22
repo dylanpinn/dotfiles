0a2b2e6939576a2cc9b9af8f4223d0d9784150ae
Dylan Pinn <dylan@arcadiandigital.com.au>
2018-01-22 14:36:54 +1100

    Fix shellcheck issue

--- iterm2/install.sh
+++ iterm2/install.sh
@@ -4,7 +4,7 @@ echo -e "\n\nSetting up iTerm"
 echo "======================="
 
 # Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/iterm2"
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$HOME/.dotfiles/iterm2"
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
