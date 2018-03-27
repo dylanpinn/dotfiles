c97c444b741cea7868fe2fa8b432e129a638398f
Dylan Pinn <dylan@dylanpinn.com>
2018-03-27 22:40:09 +1100

    [zsh] fix local config

--- zsh/local.zsh
+++ zsh/local.zsh
@@ -1,5 +1,19 @@
 #!/bin/zsh
 
+# Check if we can read given files and source those we can.
+xsource() {
+    if (( ${#argv} < 1 )) ; then
+        printf 'usage: xsource FILE(s)...\n' >&2
+        return 1
+    fi
+
+    while (( ${#argv} > 0 )) ; do
+        [[ -r "$1" ]] && source "$1"
+        shift
+    done
+    return 0
+}
+
 zrclocal() {
     xsource "/etc/zsh/zshrc.local"
     xsource "${HOME}/.zshrc.local"
