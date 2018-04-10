cc2acd0d229b138f54784964fc92a5fd3dff7180
Dylan Pinn <dylan@dylanpinn.com>
2018-04-10 21:06:55 +1000

    Start adding install script

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo -e "\\n\\nSetting up VSCode"
+echo "======================="
+
 # Symlink to more default dir if macOS
 if [[ "$OSTYPE" == darwin* ]]; then
   ln -sf $HOME/.config/Code/User/*.json "$HOME/Library/Application Support/Code/User"
