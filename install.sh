2b4056eb11eaa395bdda1066f16532b63c5b394e
Dylan Pinn <dylan@dylanpinn.com>
2019-07-19 09:58:58 +1000

    [iterm] remove config

--- install.sh
+++ install.sh
@@ -6,10 +6,5 @@ echo "Symlinking dotfiles..."
 # TODO: Check if rcup is installed
 env RCRC="$HOME/.dotfiles/rcrc" rcup -v
 
-# Setup iterm2 config
-if [[ "$OSTYPE" == darwin* ]]; then
-  . iterm2/install.sh
-fi
-
 # Setup VSCode config
 . vscode/install.sh
