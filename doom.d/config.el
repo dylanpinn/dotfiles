3e979bf7df4a3d1c2bfffcf9a91c6ae749b275c5
Dylan Pinn <dylan.pinn@rea-group.com>
2020-05-12 10:35:52 +1000

    [EMACS] Log task state changes into the drawer

--- doom.d/config.el
+++ doom.d/config.el
@@ -51,3 +51,6 @@
 ;;
 ;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
 ;; they are implemented.
+
+;; Insert state change notes and time stamps into a drawer
+(setq org-log-into-drawer t)
