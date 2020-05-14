7ff06ef73e9e3d4dce65608ff088acd22ffc32c0
Dylan Pinn <dylan.pinn@rea-group.com>
2020-05-14 17:05:11 +1000

    Update install script

--- install.sh
+++ install.sh
@@ -2,18 +2,29 @@
 
 echo "Setting up dotfiles...."
 
+echo "Installing dependencies.."
+brew bundle --verbose
+
 echo "Symlinking dotfiles..."
-# TODO: Check if rcup is installed
 env RCRC="$HOME/.dotfiles/rcrc" rcup -v
 
-# Setup VSCode config
-. vscode/install.sh
+echo "Setting up vim..."
+mkdir -p ~/.vim/{backfiles,swap,undo}
 
-yarn install
+[ ! -d "$HOME/.vim/pack/minpac/opt/minpac" ] && \
+  git clone https://github.com/k-takata/minpac.git \
+  ~/.vim/pack/minpac/opt/minpac
 
-# Setup iterm2
-# TODO: Only when on macOS.
-# Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/iterm2"
-# Tell iTerm2 to use the custom preferences in the directory
-defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+echo "Setting up nvm..."
+# NOTE: Try to keep this version up-to date.
+[ ! -d "$HOME/.nvm" ] && \
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+echo "Setting up emacs-doom..."
+[ ! -d "$HOME/.emacs.d" ] && \
+  git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d && \
+  ~/.emacs.d/bin/doom install
+
+echo "Setting up yarn..."
+[ ! -d "$HOME/.yarn" ] && \
+  curl -o- -L https://yarnpkg.com/install.sh | bash
