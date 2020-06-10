2931565495763b27bcb806e8357ecff2347212b2
Dylan Pinn <dylan.pinn@rea-group.com>
2020-06-11 09:53:50 +1000

    [BASH] Add Jobs and Colors to prompt

--- bashrc.d/prompt.bash
+++ bashrc.d/prompt.bash
@@ -1,16 +1,49 @@
 <<<<<<< HEAD
 # Prompt
 
-# ~/pwd(git-branch)$
-# TODO: Replace with native PS1 and implement colours that way.
-# PROMPT_COMMAND='__git_ps1 "\n\w" "\$ " "(%s)"'
-PS1=
-PS1=$PS1"\w"
-PS1=$PS1'$(__git_ps1)'
-PS1=$PS1'\$ '
+# If Bash 4.0 is available, trim very long paths in prompt
+if ((BASH_VERSINFO[0] >= 4)) ; then
+  PROMPT_DIRTRIM=4
+fi
+
+# git prompt settings.
 GIT_PS1_SHOWDIRTYSTATE=1       # staged '+', unstaged '*'
 GIT_PS1_SHOWSTASHSTATE=1       # '$' something is stashed
 GIT_PS1_SHOWUNTRACKEDFILES=1   # '%' untracked files
 GIT_PS1_SHOWUPSTREAM="auto"    # '<' behind, '>' ahead, '<>' diverged, '=' no difference
 GIT_PS1_DESCRIBE_STYLE=default # more info when in a detached HEAD
-# GIT_PS1_SHOWCOLORHINTS=1       # Display colours in PROMPT_COMMAND
+
+prompt_jobs() {
+  local jobc
+  while read -r _; do
+    ((jobc++))
+  done < <(jobs -p)
+  if ((jobc > 0)); then
+    printf '{%d}' "$jobc"
+  fi
+}
+
+# ~/pwd(git-branch){job-count}$
+prompt_on() {
+  PS1=
+  PS1=$PS1"\w"
+  PS1=$PS1'$(__git_ps1)'
+  PS1=$PS1'$(prompt_jobs)'
+  PS1=$PS1'\$'
+  PS1=$PS1' '
+
+  local color_prompt
+  color_prompt=$(tput setaf 2)
+  local color_reset
+  color_reset=$(tput sgr0)
+
+  PS1="\\[$color_prompt\\]$PS1\\[$color_reset\\]"
+}
+
+# $
+prompt_off() {
+  PS1='\$'
+  PS1=$PS1' '
+}
+
+prompt_on
