1c3fa9e979ce1961068b514baee75d19576c0190
Dylan Pinn <me@dylanpinn.com>
2020-06-12 01:30:39 +1000

    Add shellcheck to CI and script to run it

---
+++ lint-shell.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+shellcheck profile install.sh lint-shell.sh lint-vim.sh bash_completion \
+  bash_profile bashrc
+
+for bcfile in bash_completion.d/*; do
+  [ -f "$bcfile" ] && shellcheck $bcfile
+done
+unset -v bcfile
+
+for bash in bashrc.d/*.bash ; do
+  [[ -e $bash ]] || continue
+  shellcheck "$bash"
+done
+unset -v bash
