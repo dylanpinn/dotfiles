449c2e35799216ba935132d507bd58a22d4e7b88
Dylan Pinn <me@dylanpinn.com>
2020-06-12 02:01:27 +1000

    Specify shell in script

--- lint-shell.sh
+++ lint-shell.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-shellcheck -e SC1090 profile install.sh lint-shell.sh lint-vim.sh \
+shellcheck -e SC1090 -s bash profile install.sh lint-shell.sh lint-vim.sh \
   bash_completion bash_profile bashrc
 
 for bcfile in bash_completion.d/*; do
-  [ -f "$bcfile" ] && shellcheck -e SC1090 "$bcfile"
+  [ -f "$bcfile" ] && shellcheck -e SC1090 -s bash "$bcfile"
 done
 unset -v bcfile
 
 for bash in bashrc.d/*.bash ; do
   [[ -e $bash ]] || continue
-  shellcheck -e SC1090 "$bash"
+  shellcheck -e SC1090 -s bash "$bash"
 done
 unset -v bash
