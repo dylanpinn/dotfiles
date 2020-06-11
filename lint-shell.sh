42911735125fbeca2fc8cbb8d7dc4e272d6623d5
Dylan Pinn <me@dylanpinn.com>
2020-06-12 01:56:11 +1000

    Don't check SC1090

--- lint-shell.sh
+++ lint-shell.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-shellcheck profile install.sh lint-shell.sh lint-vim.sh bash_completion \
-  bash_profile bashrc
+shellcheck -e SC1090 profile install.sh lint-shell.sh lint-vim.sh \
+  bash_completion bash_profile bashrc
 
 for bcfile in bash_completion.d/*; do
-  [ -f "$bcfile" ] && shellcheck $bcfile
+  [ -f "$bcfile" ] && shellcheck -e SC1090 "$bcfile"
 done
 unset -v bcfile
 
 for bash in bashrc.d/*.bash ; do
   [[ -e $bash ]] || continue
-  shellcheck "$bash"
+  shellcheck -e SC1090 "$bash"
 done
 unset -v bash
