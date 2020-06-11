482fb799e7552c9503b252ed9639ee5a73ecd375
Dylan Pinn <me@dylanpinn.com>
2020-06-12 01:56:35 +1000

    [BASH] Fix shellcheck errors

--- bashrc.d/functions.bash
+++ bashrc.d/functions.bash
@@ -3,15 +3,17 @@
 # Lazy load nvm
 nvm() {
   [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"                    # This loads nvm
-  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
-  nvm $@
+  :w
+  :w
+  nvm "$@"
 }
 
 # Load JDK version.
 # e.g. jdk 1.8; jdk 11
 jdk() {
   version=$1
-  export JAVA_HOME=$(/usr/libexec/java_home -v"$version");
+  JAVA_HOME=$(/usr/libexec/java_home -v"$version");
+  export JAVA_HOME
   java -version
 }
 
