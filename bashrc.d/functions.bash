47927fff9cf73816fe95b0dc8a7a474deb6495e5
Dylan Pinn <me@dylanpinn.com>
2020-06-12 01:56:35 +1000

    [BASH] Fix shellcheck errors

--- bashrc.d/functions.bash
+++ bashrc.d/functions.bash
@@ -4,14 +4,15 @@
 nvm() {
   [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"                    # This loads nvm
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
-  nvm $@
+  nvm "$@"
 }
 
 # Load JDK version.
 # e.g. jdk 1.8; jdk 11
 jdk() {
   version=$1
-  export JAVA_HOME=$(/usr/libexec/java_home -v"$version");
+  JAVA_HOME=$(/usr/libexec/java_home -v"$version");
+  export JAVA_HOME
   java -version
 }
 
--- bashrc.d/prompt.bash
+++ bashrc.d/prompt.bash
@@ -6,10 +6,15 @@ if ((BASH_VERSINFO[0] >= 4)) ; then
 fi
 
 # git prompt settings.
+# shellcheck disable=SC2034
 GIT_PS1_SHOWDIRTYSTATE=1       # staged '+', unstaged '*'
+# shellcheck disable=SC2034
 GIT_PS1_SHOWSTASHSTATE=1       # '$' something is stashed
+# shellcheck disable=SC2034
 GIT_PS1_SHOWUNTRACKEDFILES=1   # '%' untracked files
+# shellcheck disable=SC2034
 GIT_PS1_SHOWUPSTREAM="auto"    # '<' behind, '>' ahead, '<>' diverged, '=' no difference
+# shellcheck disable=SC2034
 GIT_PS1_DESCRIBE_STYLE=default # more info when in a detached HEAD
 
 prompt_jobs() {
