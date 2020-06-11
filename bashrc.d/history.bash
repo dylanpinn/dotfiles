f9edb8ff2fc945ce69742415918d1b2df8aa1548
Dylan Pinn <me@dylanpinn.com>
2020-06-11 20:53:34 +1000

    [BASH] Add bash config

---
+++ bashrc.d/history.bash
@@ -0,0 +1,9 @@
+# History
+
+# Increase history size.
+HISTFILESIZE=1000000
+HISTSIZE=1000000
+# Ignore duplicates and commands that begin with whitespace.
+HISTCONTROL=ignoreboth 
+# Ignore certain commands.
+HISTIGNORE='ls:bg:fg:history'
