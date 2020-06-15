1f021116a051aa319fd28a4d4227dea09f99fb45
Dylan Pinn <dylan.pinn@rea-group.com>
2020-06-15 17:11:50 +1000

    [BASH] Correctly escape arguments in nvm func

--- bashrc.d/functions.bash
+++ bashrc.d/functions.bash
@@ -4,7 +4,7 @@
 nvm() {
   [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"                    # This loads nvm
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
-  nvm $@
+  nvm "$@"
 }
 
 # Load JDK version.
