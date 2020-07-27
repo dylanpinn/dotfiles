f208950cce999071f43997e3e25b8643109e3e79
Dylan Pinn <me@dylanpinn.com>
2020-07-28 09:50:56 +1000

    Disable shellcheck SC1090

--- bashrc.d/functions.bash
+++ bashrc.d/functions.bash
@@ -2,8 +2,9 @@
 
 # Lazy load nvm
 nvm() {
-  # shellcheck source=~/.nvm/nvm.sh
+  # shellcheck source=/dev/null
   [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"                    # This loads nvm
+  # shellcheck source=/dev/null
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
   nvm "$@"
 }
