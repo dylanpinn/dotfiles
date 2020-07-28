4fdc8b2ec649af29ddc0681580d99c2b57fd25b7
Dylan Pinn <dylan.pinn@gmail.com>
2020-07-28 10:16:21 +1000

    Add SuperLinter checks (#117)
    SuperLinter will check most of the configuration files, except for extensionless shell config.

--- bashrc.d/functions.bash
+++ bashrc.d/functions.bash
@@ -2,7 +2,9 @@
 
 # Lazy load nvm
 nvm() {
+  # shellcheck disable=SC1090
   [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"                    # This loads nvm
+  # shellcheck disable=SC1090
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
   nvm "$@"
 }
--- lint-shell.sh
+++ lint-shell.sh
@@ -2,16 +2,4 @@
 
 set -e
 
-shellcheck -e SC1090 -s bash profile install.sh lint-shell.sh bash_profile \
-  bashrc
-
-for bcfile in bash_completion.d/*; do
-  [ -f "$bcfile" ] && shellcheck -e SC1090 -s bash "$bcfile"
-done
-unset -v bcfile
-
-for bash in bashrc.d/*.bash ; do
-  [[ -e $bash ]] || continue
-  shellcheck -e SC1090 -s bash "$bash"
-done
-unset -v bash
+shellcheck -e SC1090 -s bash profile bash_profile bashrc
