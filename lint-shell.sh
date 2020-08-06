ccb9f6d907457505eae90092a0a479e918a4857d
Dylan Pinn <dylan.pinn@gmail.com>
2020-08-06 15:08:36 +1000

    [GIT] Add divergent command (#122)
    * [GIT] Add divergence command
    
    Shows incomming & outgoing changes between branches.
    
    * Run lint-shell on bin script files.
    
    * Start fixing lint issues

--- lint-shell.sh
+++ lint-shell.sh
@@ -2,4 +2,9 @@
 
 set -e
 
+# Shell configuration files.
 shellcheck -s bash profile bash_profile bashrc
+
+# Shell scripts in bin directory.
+shellcheck -e SC1090 -s bash bin/git-divergence
+shellcheck -e SC1090 -s bash bin/run-command-on-git-revisions
