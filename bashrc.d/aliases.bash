9c29166d333a0da6bfbdf437c5a43a67d92a3b7d
Dylan Pinn <dylan.pinn@rea-group.com>
2020-09-04 16:23:44 +1000

    [BASH] Update test-chagned alias to use native jest functionality

--- bashrc.d/aliases.bash
+++ bashrc.d/aliases.bash
@@ -4,5 +4,5 @@
 alias ls="ls -G"
 
 # Run Jest tests against changed files.
-alias test-changed='npx jest --findRelatedTests $(git diff --name-only master)'
+alias test-changed='npx jest --changedSince master'
 alias lint-changed='npx eslint $(git diff --name-only master | grep "\.js$")'
