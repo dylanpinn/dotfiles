53c7d53152578a7d2a1c3cafbfe0bb5e2b6c9f49
Dylan Pinn <dylan.pinn@rea-group.com>
2021-02-18 16:36:38 +1100

    feat(neovim): add keybinding for showing all LSP references

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -60,6 +60,7 @@ map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
 map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
 map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
 map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
 
 ----------------------------------
 -- COMMANDS ----------------------
