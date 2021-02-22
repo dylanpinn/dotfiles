8098848b36f853db1ed96bea4e9723f0a653db7d
Dylan Pinn <dylan.pinn@gmail.com>
2021-02-22 10:55:00 +1000

    feat(bash): create bash_completion directory (#169)
    Allow setting custom bash completion scripts in dotfiles.
    
    Also use brew prefix for bash_completion script.

--- bashrc.d/completion.bash
+++ bashrc.d/completion.bash
@@ -1,3 +1,3 @@
 # Bash completion.
-# shellcheck disable=SC1091
-[[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
+# shellcheck source=/dev/null
+[[ -r "$(brew --prefix)/etc/profile.d/bash_completion.sh" ]] && . "$(brew --prefix)/etc/profile.d/bash_completion.sh"
