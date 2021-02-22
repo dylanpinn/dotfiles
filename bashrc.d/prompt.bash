0751a788838fdaf315ae109b615c8887422922c6
Dylan Pinn <dylan.pinn@gmail.com>
2021-02-23 08:39:22 +1000

    feat(bash): add Git info to prompt (#182)

--- bashrc.d/prompt.bash
+++ bashrc.d/prompt.bash
@@ -1,3 +1,15 @@
 # Prompt
 
-PS1='\w\$ '
+# git prompt settings.
+# shellcheck disable=SC2034
+GIT_PS1_SHOWDIRTYSTATE=1 # staged '+', unstaged '*'
+# shellcheck disable=SC2034
+GIT_PS1_SHOWSTASHSTATE=1 # '$' something is stashed
+# shellcheck disable=SC2034
+GIT_PS1_SHOWUNTRACKEDFILES=1 # '%' untracked files
+# shellcheck disable=SC2034
+GIT_PS1_SHOWUPSTREAM="auto" # '<' behind, '>' ahead, '<>' diverged, '=' no difference
+# shellcheck disable=SC2034
+GIT_PS1_DESCRIBE_STYLE=default # more info when in a detached HEAD
+
+PS1='\w$(__git_ps1)\$ '
