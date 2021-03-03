cdc7c858137577809d8e0177bf53ec9ee0a27e24
Dylan Pinn <dylan.pinn@rea-group.com>
2021-03-03 14:36:27 +1100

    feat(bash): update prompt
    Update prompt to include git info, NodeJS version and Job information.

--- bashrc.d/prompt.bash
+++ bashrc.d/prompt.bash
@@ -1,5 +1,10 @@
 # Prompt
 
+# If Bash 4.0 is available, trim very long paths in prompt
+if ((BASH_VERSINFO[0] >= 4)); then
+  PROMPT_DIRTRIM=4
+fi
+
 # git prompt settings.
 # shellcheck disable=SC2034
 GIT_PS1_SHOWDIRTYSTATE=1 # staged '+', unstaged '*'
@@ -12,4 +17,59 @@ GIT_PS1_SHOWUPSTREAM="auto" # '<' behind, '>' ahead, '<>' diverged, '=' no diffe
 # shellcheck disable=SC2034
 GIT_PS1_DESCRIBE_STYLE=default # more info when in a detached HEAD
 
-PS1='\w$(__git_ps1)\$ '
+PROMPT_SHOW_NVM=0
+function check_prompt() {
+  if hash node 2>/dev/null; then
+    PROMPT_SHOW_NVM=1
+  fi
+}
+
+# activating it
+export PROMPT_COMMAND=check_prompt
+
+prompt_jobs() {
+  local jobc
+  while read -r _; do
+    ((jobc++))
+  done < <(jobs -p)
+  if ((jobc > 0)); then
+    printf '{%d}' "$jobc"
+  fi
+}
+
+function __node() {
+  if [[ $PROMPT_SHOW_NVM = 1 ]]; then
+    local v
+    v=$(node -v)
+    [ "$v" != "" ] && echo "[n:${v:1}]"
+  fi
+}
+
+# ~/pwd(git-branch){job-count}[versions]$
+prompt_on() {
+  PS1=
+  PS1=$PS1"\w"
+  PS1=$PS1'$(__git_ps1)'
+  PS1=$PS1'$(prompt_jobs)'
+  PS1=$PS1'$(__node)'
+  PS1=$PS1'\$'
+  PS1=$PS1' '
+
+  local color_prompt
+  color_prompt=$(tput bold)
+  # Set colour to TEAL.
+  color_prompt=$color_prompt$(tput setaf 6)
+  local color_reset
+  color_reset=$(tput sgr0)
+
+  export PS1="\\[$color_prompt\\]$PS1\\[$color_reset\\]"
+}
+
+# $
+prompt_off() {
+  PS1='\$'
+  PS1=$PS1' '
+  export PS1
+}
+
+prompt_on
