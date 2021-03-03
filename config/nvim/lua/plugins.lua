15482e8a6495ba02adaabcc6de25087390958fc1
Dylan Pinn <dylan.pinn@rea-group.com>
2021-03-03 13:31:30 +1100

    style(nvim): sort plugins alphabetically

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,10 +1,10 @@
 require('packer').startup(function(use)
   use {'wbthomason/packer.nvim', opt = true}
 
-  use {'wakatime/vim-wakatime'}
+  use {'ajh17/VimCompletesMe'}
   use {'dracula/vim', as = 'dracula'}
   use {'neovim/nvim-lspconfig'}
   use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
-  use {'ajh17/VimCompletesMe'}
+  use {'wakatime/vim-wakatime'}
 end)
 
