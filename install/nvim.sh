28a069254b67905fc94bff7b3fb97af63ab7a530
Dylan Pinn <me@dylanpinn.com>
2021-03-21 22:05:43 +1100

    setup editorconfig for neovim

--- install/nvim.sh
+++ install/nvim.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Check if packer has been setup
-if [ ! -d "$HOME/.local/share/nvim/site/pack/packer/opt/packer" ]; then
+if [ ! -d "$HOME/.local/share/nvim/site/pack/packer/opt/packer.nvim" ]; then
   # If not install it
   git clone https://github.com/wbthomason/packer.nvim \
-    ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+    ~/.local/share/nvim/site/pack/packer/opt/packer.nvim
 fi
