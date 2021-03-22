5a2f78a66e3e3687ba962fb7d083a42dd6b7a006
Dylan Pinn <dylan.pinn@rea-group.com>
2021-03-22 14:20:33 +1100

    Add treesitter to nvim

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -6,8 +6,17 @@ return require('packer').startup(function(use)
   -- Packer can manage itself as an optional plugin
   use {'wbthomason/packer.nvim', opt = true}
 
+  -- Color scheme
   use {'dracula/vim', as = 'dracula'}
 
-  use {'wakatime/vim-wakatime'}
+  -- Apply consisistent editor settings for a project.  Tracked in
+  -- a .editorconfig file.
   use {'editorconfig/editorconfig-vim', opt = true, as = 'editorconfig'}
+
+  -- Use treesitter to improve syntax highlighting.
+  use {'nvim-treesitter/nvim-treesitter'}
+  require('settings/nvim-treesitter')
+
+  -- Track development effort across projects.
+  use {'wakatime/vim-wakatime'}
 end)
