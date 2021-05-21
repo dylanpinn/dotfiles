bfff4546f868fda80e7d9c0c35e80b4ef8bfcc85
Dylan Pinn <dylan.pinn@rea-group.com>
2021-05-21 16:10:36 +1000

    Fix install

--- install/nvm.sh
+++ install/nvm.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-NVM_DIR="$XDG_DATA_HOME/nvm"
+NVM_DIR="${XDG_DATA_HOME:$HOME/.local/share}/nvm"
 # Check if NVM has been installed
 if [ ! -d "$NVM_DIR" ]; then
   # If not install it
