9b2c49ad78ebe5bae631434590312199cf0119f2
Dylan Pinn <dylan.pinn@rea-group.com>
2021-09-01 17:53:17 +1000

    [emacs] add Docker and Spellcheck

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -80,14 +80,14 @@
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       ;;(spell +flyspell) ; tasing you for misspelling mispelling
+       (spell +flyspell) ; tasing you for misspelling mispelling
        ;;grammar           ; tasing grammar mistake every you make
 
        :tools
        ;;ansible
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
-       ;;docker
+       docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
