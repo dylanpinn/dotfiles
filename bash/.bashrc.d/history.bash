d75d4e38c5014ae08008f1f1b23d7675fe30b187
Dylan Pinn <me@dylanpinn.com>
2021-09-12 15:25:19 +1000

    Remove all files

--- bash/.bashrc.d/history.bash
+++
@@ -1,9 +0,0 @@
-# History
-
-# Increase history size.
-HISTFILESIZE=1000000
-HISTSIZE=1000000
-# Ignore duplicates and commands that begin with whitespace.
-HISTCONTROL=ignoreboth
-# Ignore certain commands.
-HISTIGNORE='ls:bg:fg:history'
