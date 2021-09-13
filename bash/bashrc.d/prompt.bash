5c6d080e0f59365c4473ea9036b51fa2621669f0
Dylan Pinn <dylan.pinn@rea-group.com>
2021-09-13 10:34:11 +1000

    refactor(bash): move config out to bashrc.d

---
+++ bash/bashrc.d/prompt.bash
@@ -0,0 +1,9 @@
+# If Bash 4.0 is available, trim very long paths in prompt.
+if ((BASH_VERSINFO[0] >= 4)); then
+  PROMPT_DIRTRIM=4
+fi
+
+# Set prompt to be user:~/pwd$
+# - user is bold green
+# - path is bold blue
+PS1='\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
