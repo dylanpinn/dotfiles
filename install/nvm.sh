d7fb1fb135f47442ff36d6e681e1448f64e73ab6
Dylan Pinn <dylan.pinn@rea-group.com>
2021-09-13 10:54:41 +1000

    feat(nvm): install nvm and config
    Move config out of $HOME and lazy load.

---
+++ install/nvm.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+XDG_DATA_HOME="$HOME"/.local/share
+NVM_DIR="${XDG_DATA_HOME}/nvm"
+# Check if NVM has been installed
+if [ ! -d "$NVM_DIR" ]; then
+  # If not install it
+  (
+    git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+  )
+fi
+
+# shellcheck source=/dev/null
+(
+  cd "$NVM_DIR"
+  git fetch --tags origin
+  git checkout "$(git describe --abbrev=0 --tags --match "v[0-9]*" "$(git rev-list --tags --max-count=1)")"
+) && \. "$NVM_DIR/nvm.sh"
+echo "nvm setup"
