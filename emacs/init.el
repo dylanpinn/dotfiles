ee5bba08c5f90cbc6c5a8f72f4f503650fbdbd58
Dylan Pinn <dylan.pinn@rea-group.com>
2021-09-13 13:49:18 +1000

    style(emacs): clean up configuration

--- emacs/init.el
+++ emacs/init.el
@@ -1,55 +1,36 @@
-; UI - Clean up
+;; UI - Clean up
 
-; Remove startup message
+;; Remove startup message
 (setq inhibit-startup-message t)
 
-; Disable visible scrollbar
+;; Disable visible scrollbar
 (scroll-bar-mode -1)
 
-; Disable tooltips
+;; Disable tooltips
 (tooltip-mode -1)
 
-; Disable the toolbar
+;; Disable the toolbar
 (tool-bar-mode -1)
 
-; Give some breathing room
+;; Give some breathing room
 (set-fringe-mode 10)
 
-; Disable the menu bar
+;; Disable the menu bar
 (menu-bar-mode -1)
 
-; Set up the visible bell
+;; Set up the visible bell
 (setq visible-bell t)
 
-; Make ESC quit prompts
+;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-; Custom Package sources
+;; Load built-in package manager, add community sources.
 (require 'package)
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
-
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
 
-; Initialise use-package
+;; bootstrap use-package
 (unless (package-installed-p 'use-package)
+  (package-refresh-contents)
   (package-install 'use-package))
-
 (require 'use-package)
-(setq use-package-always-ensure t)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
