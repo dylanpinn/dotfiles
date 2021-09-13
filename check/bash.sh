fed564ffd83d5f9e29ef49a77ae781044f0c139f
Dylan Pinn <dylan.pinn@rea-group.com>
2021-09-14 09:43:09 +1000

    chore: add check script for bash files

---
+++ check/bash.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Check syntax for bash files.
+for bash in bash/* */bashrc.d/* ; do
+  [ -f "$bash" ] || continue
+  bash -n "$bash" || exit
+done
+
+printf 'All bash(1) scripts parsed successfully.\n'
