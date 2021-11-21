418b37a174d2b8b8207668eae64488a9b44eb4ce
Dylan Pinn <dylan.pinn@rea-group.com>
2021-11-22 10:39:23 +1100

    [emacs] Imrove UI
    - Add line numbers.
    - Improve ivy with ivy-rich
    - Add which-key
    - Use Palenight theme
    - Add helpful

--- emacs/init.el
+++ emacs/init.el
@@ -16,9 +16,15 @@
 (menu-bar-mode -1)
 ;; Set the visual bell
 (setq visible-bell t)
-
-;; Basic theme
-(load-theme 'wombat)
+;; Line numbers
+(column-number-mode)
+(global-display-line-numbers-mode t)
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		eshell-mode-hook
+		shell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
@@ -56,8 +62,44 @@
 (use-package swiper
   :after ivy
   :bind (("C-s" . swiper)))
+(use-package ivy-rich
+  :after ivy
+  :config
+  (ivy-rich-mode 1)
+  (setcdr (assq t ivy-format-functions-alist) #'ivy-format-function-line))
 
 ;;; Doom Modeline
 (use-package doom-modeline
-  :init
+  :config
   (doom-modeline-mode 1))
+
+;;; Doom Themes
+(use-package doom-themes
+  :config
+  (load-theme 'doom-palenight)
+  (doom-themes-visual-bell-config)
+  (doom-themes-org-config))
+
+;;; Rainbow Delimiters
+(use-package rainbow-delimiters
+  :hook
+  (prog-mode . rainbow-delimiters-mode))
+
+;;; which-key
+(use-package which-key
+  :diminish which-key-mode
+  :config
+  (which-key-mode)
+  (setq which-key-idle-delay 0.3))
+
+;;; Helpful
+(use-package helpful
+  :commands (helpful-callable helpful-variable helpful-key helpful-at-point
+			      helpful-function helpful-command)
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind (([remap describe-key] . helpful-key)
+	 ("C-c C-d" . helpful-at-point)
+	 ([remap describe-function] . helpful-function)
+	 ([remap describe-command] . helpful-command)))
