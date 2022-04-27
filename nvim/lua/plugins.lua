cf0ad456ab327703cde9674fb948accdab7268b5
Dylan Pinn <dylan.pinn@rea-group.com>
2022-04-27 15:25:18 +1000

    feat(nvim): add config for metals-scala
    - Example config that will work for Scala using Metals.

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,4 +1,11 @@
 return require('packer').startup(function()
   -- Use Packer to manage it self
   use 'wbthomason/packer.nvim'
+
+  -- Use Metals for Scala development.
+  use {
+    'scalameta/nvim-metals',
+    requires = { "nvim-lua/plenary.nvim" },
+    config = 'vim.opt_global.shortmess:remove("F")'
+  }
 end)
