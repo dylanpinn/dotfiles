1a7aad598ba0c0cdcd5272ba28581e2dce847c9e
Dylan Pinn <dylan.pinn@rea-group.com>
2022-04-28 10:08:24 +1000

    docs(nvim): add more documentation for Neovim script

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,5 @@
+-- This is my Neovim configuration.  I am trying to replace Vim with Neovim so
+-- I can get native LSP support for TypeScript and Scala projects.
+
+-- Use the built in desert color scheme until I find something I like more.
 vim.cmd("colorscheme desert")
