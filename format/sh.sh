17f1dd8f7c41284ceef1fb15d413c2392d0f9eaa
Dylan Pinn <dylan.pinn@rea-group.com>
2022-04-28 10:37:46 +1000

    style(sh): use EditorConfig rather than hardcoding config

--- format/sh.sh
+++ format/sh.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env sh
 #
 # Format shell files.
+#
+# Will use configuration in .editorconfig file:
+# - Use 2 spaces for indent.
+# - Use POSIX style.
+# - Switch cases will be indented.
+# - Binary ops like && and | may start a line.
 
 set -e
 
 set sh/profile
 
-shfmt -l -w -i 2 -ln posix -ci -bn -- "$@"
+shfmt -l -w -- "$@"
 
 find . -type f -name "*.sh" | while IFS='' read -r line; do
-  shfmt -l -w -i 2 -ln posix -ci -bn -- "$line"
+  shfmt -l -w -- "$line"
 done
