65fbfa87fd9ff6b102a2000472afe010ccbe0fdf
Dylan Pinn <dylan.pinn@rea-group.com>
2022-10-17 15:30:36 +1100

    feat(nvim): add mason-lspconfig for auto installing
    - Add recommended LSP configuration and tsserver config

--- nvim/lua/dcp/packages.lua
+++ nvim/lua/dcp/packages.lua
@@ -3,5 +3,6 @@
 return require("paq")({
   "savq/paq-nvim", -- Let Paq manage itself
   "williamboman/mason.nvim", -- Portable package manager for Neovim.
+  "williamboman/mason-lspconfig.nvim", -- Extension for Mason.nvim that makes it easier to use lspconfig.
   "neovim/nvim-lspconfig", -- Collection of configurations for the built-in LSP client.
 })
