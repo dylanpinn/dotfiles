978c63b3602d7863a3d694467d2ade018ed1571d
Dylan Pinn <dylan.pinn@rea-group.com>
2022-10-17 16:00:47 +1100

    feat(nvim): use null-ls to enhance LSP client
    - Allow linters and formatters to use LSP client.
    - Setup stylua and luacheck with null-ls.

--- nvim/lua/dcp/packages.lua
+++ nvim/lua/dcp/packages.lua
@@ -5,4 +5,6 @@ return require("paq")({
   "williamboman/mason.nvim", -- Portable package manager for Neovim.
   "williamboman/mason-lspconfig.nvim", -- Extension for Mason.nvim that makes it easier to use lspconfig.
   "neovim/nvim-lspconfig", -- Collection of configurations for the built-in LSP client.
+  "nvim-lua/plenary.nvim", -- Lua functions. null-ls requires this.
+  "jose-elias-alvarez/null-ls.nvim", -- Allow linters & formatters to connect to LSP client.
 })
