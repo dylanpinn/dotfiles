256018d7883f9cd880de5c2b6b740bbf16dd230e
Dylan Pinn <dylan.pinn@rea-group.com>
2022-12-08 16:53:09 +1100

    feat(nvim): setup basic LSP using lspconfig

---
+++ nvim/lua/dcp/keymaps.lua
@@ -0,0 +1,21 @@
+-- Mapping defnitions are defined here.  Here are some rules I have tried to
+-- follow as much as possible.
+--
+-- * Use Leader key as a prefix for mappings as much as possible.
+--
+-- * Only use the LocalLeader key as a prefix for mappings that are defined as
+-- local to a buffer, which should always be based on filetype and be setup in
+-- ftplugin files.
+--
+-- * Avoid shadowing any of Vim's existing functionality.  If possible, extend
+-- or supplement what Vim does, rather than replacing it.
+--
+-- The following are mappings that do not use the Leader key.
+
+-- The idea for this is taken from tpop/vim-vinegar.  Being able to hit "-" and
+-- be taken automatically the files directory in netrw is very helpful and much
+-- easier than typing ":Ex".  Only stealing this keybinding at the moment
+-- (definitely not as powerful as the original) as don't require the rest of
+-- the plugin (currently).
+--
+vim.keymap.set("n", "-", ":Ex<CR>")
