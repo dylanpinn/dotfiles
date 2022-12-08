424feb468938efde9b2f5aa908783b47d1110f0a
Dylan Pinn <dylan.pinn@rea-group.com>
2022-12-08 14:16:37 +1100

    feat(nvim): add Paq and dcp module

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,9 @@
 -- This is my personal Neovim config.
+--
+-- It is intentionally very bare bones and using the features of Neovim that
+-- are missing from Vim, i.e. LSP and treesitter.
+
+-- The configuration is located in the dcp module (located in the lua
+-- directory). It is then split up based on the type of configuration to make
+-- small files rather than a single large file (vimrc).
+require('dcp')
