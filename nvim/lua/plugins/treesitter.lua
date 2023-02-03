d9ae4aa67bb393a5710c7cccd9641b18726bd148
Dylan Pinn <dylan.pinn@rea-group.com>
2023-02-03 11:00:11 +1100

    feat(nvim): add nvim-treesitter
    Migrate configuration from after/plugin/treesitter.lua to be loaded
    through lazy.nvim.

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+      vim.cmd("TSUpdate")
+    end,
+    event = "BufReadPost",
+    opts = {
+      -- A list of parser names, or "all"
+      ensure_installed = { "help", "lua", "vim" },
+
+      -- Install parsers synchronously (only applied to `ensure_installed`)
+      sync_install = false,
+
+      -- Automatically install missing parsers when entering buffer
+      -- Recommendation: set to false if you don't have `tree-sitter` CLI
+      -- installed locally
+      auto_install = true,
+
+      highlight = {
+        enable = true,
+
+        -- Setting this to true will run `:h syntax` and tree-sitter at the
+        -- same time. Set this to `true` if you depend on 'syntax' being
+        -- enabled (like for indentation). Using this option may slow down your
+        -- editor, and you may see some duplicate highlights. Instead of true
+        -- it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
+}
+
