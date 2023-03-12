bd2c7e443919da99b27e8a0716503ca9eb6ef6a4
Dylan Pinn <me@dylanpinn.com>
2023-03-12 23:44:38 +1100

    TODO - Make completion work on Linux

--- bash/bashrc.d/completion.bash
+++ bash/bashrc.d/completion.bash
@@ -1,3 +1,4 @@
 # Use bash completion from Homebrew.
-[[ -r "$(brew --prefix)/etc/profile.d/bash_completion.sh" ]] \
-  && . "$(brew --prefix)/etc/profile.d/bash_completion.sh"
+# TODO: Only run this on macOS.
+# [[ -r "$(brew --prefix)/etc/profile.d/bash_completion.sh" ]] \
+#   && . "$(brew --prefix)/etc/profile.d/bash_completion.sh"
