b62609c53ce132498c7751127c241285a3739a86
Dylan Pinn <me@dylanpinn.com>
2023-03-28 23:13:14 +1100

    xinit not working

--- x/profile.d/xinit.sh
+++ x/profile.d/xinit.sh
@@ -1,4 +1,4 @@
 # Move xinit config out of $HOME directory.
-export XINITRC="$XDG_CONFIG_HOME"/X11/xinitrc
-export XSERVERRC="$XDG_CONFIG_HOME"/X11/xserverrc
-export XAUTHORITY="$XDG_RUNTIME_DIR"/Xauthority
+# export XINITRC="$XDG_CONFIG_HOME"/X11/xinitrc
+# export XSERVERRC="$XDG_CONFIG_HOME"/X11/xserverrc
+# export XAUTHORITY="$XDG_RUNTIME_DIR"/Xauthority
