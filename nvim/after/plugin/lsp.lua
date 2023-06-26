41b68ccefc4bca03cefb560e42652db169dd3113
Dylan Pinn <dylan.pinn@rea-group.com>
2023-06-26 10:44:07 +1000

    feat(nvim): fix issue with LSP and format file

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -3,38 +3,38 @@ require("mason-lspconfig").setup()
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set('n', '<localleader>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<localleader>q', vim.diagnostic.setloclist)
+vim.keymap.set("n", "<localleader>e", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<localleader>q", vim.diagnostic.setloclist)
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
   callback = function(ev)
     -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<localleader>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<localleader>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set('n', '<localleader>wl', function()
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "<localleader>wa", vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set("n", "<localleader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set("n", "<localleader>wl", function()
       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, opts)
-    vim.keymap.set('n', '<localleader>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<localleader>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set({ 'n', 'v' }, '<localleader>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<localleader>f', function()
-      vim.lsp.buf.format { async = true }
+    vim.keymap.set("n", "<localleader>D", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "<localleader>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set({ "n", "v" }, "<localleader>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<localleader>f", function()
+      vim.lsp.buf.format({ async = true })
     end, opts)
   end,
 })
@@ -45,8 +45,7 @@ require("mason-lspconfig").setup_handlers({
   -- and will be called for each installed server that doesn't have
   -- a dedicated handler.
   function(server_name) -- default handler (optional)
-    require("lspconfig")[server_name].setup({
-    })
+    require("lspconfig")[server_name].setup({})
   end,
   -- Lua LS requires additional configuration.
   ["lua_ls"] = function()
@@ -76,12 +75,12 @@ require("mason-lspconfig").setup_handlers({
   end,
   -- ltex-ls requires additional configuration.
   ["ltex"] = function()
-    require('lspconfig').ltex.setup({
+    require("lspconfig").ltex.setup({
       settings = {
         ltex = {
           language = "en-AU",
-          languageToolHttpServerUri = "http://localhost:8081/"
-        }
+          languageToolHttpServerUri = "http://localhost:8081/",
+        },
       },
     })
   end,
@@ -94,6 +93,7 @@ require("mason-null-ls").setup({
   },
   automatic_installation = false,
   automatic_setup = true, -- Recommended, but optional
+  handlers = {},
 })
 require("null-ls").setup({
   sources = {
