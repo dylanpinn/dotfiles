3df91cdd5bce34d9366733d06f1ce0e853fd60f5
Dylan Pinn <dylan.pinn@rea-group.com>
2023-09-01 12:25:53 +1000

    feat(brew): get brew working with global file and multiple machines
    Use global, ~/.Brewfile to read from when applying updates, however, we
    are dumping to .Brewfile_work and only including that on particular
    machines.
    
    Remove previous implementation as well.

---
+++ home/.chezmoiscripts/run_after_update-homebrew-bundle.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+brew bundle dump --force --describe --file=~/.local/share/chezmoi/home/.Brewfile_work
--- home/.chezmoiscripts/run_onchange_before_install-packages-darwin.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Install packages from global ~/.Brewfile
-brew bundle --global --no-lock
