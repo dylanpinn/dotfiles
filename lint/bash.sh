cd53a8e1c0e34f4ce589e633d6465ba6ea95170e
Dylan Pinn <me@dylanpinn.com>
2023-09-01 16:38:00 +1000

    fix(lint): fix shellcheck linting for chezmoi scripts
    These are bash scripts, for now, so checking with lint-bash.

--- lint/bash.sh
+++ lint/bash.sh
@@ -7,3 +7,7 @@ set -e
 # TODO: Find a way to remove this.
 # shellcheck disable=SC2046
 shellcheck -e SC1090 -e SC1091 -s bash -- $(find . -type f -name "*.bash")
+
+# TODO: Find a way to remove this.
+# shellcheck disable=SC2046
+shellcheck -e SC1090 -e SC1091 -s bash -- $(find . -type f -name "*.sh" -path "./home/.chezmoiscripts/*")
