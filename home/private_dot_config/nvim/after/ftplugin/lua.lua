6225d0d9035cd004f48cac47da7d9ac61afdabb7
Dylan Pinn <dylan.pinn@rea-group.com>
2023-09-04 22:43:44 +1000

    feat(nvim): setup stylua as formatprg for lua
    This will help to remove an extra dependency from Mason and null-ls.

--- home/private_dot_config/nvim/after/ftplugin/lua.lua
+++ home/private_dot_config/nvim/after/ftplugin/lua.lua
@@ -1,2 +1,7 @@
 -- Set maximum text width to 120 chars.
 vim.opt_local.textwidth = 120
+
+-- Use stylua as formatter if it is available.
+if vim.fn.executable("stylua") == 1 then
+  vim.opt_local.formatprg = "stylua --stdin-filepath % --search-parent-directories -"
+end
