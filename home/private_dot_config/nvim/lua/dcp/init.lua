f0532076ed47106f2248721de2a9c929fb28477a
Dylan Pinn <me@dylanpinn.com>
2023-12-03 23:14:21 +1100

    style: format lua comments

--- home/private_dot_config/nvim/lua/dcp/init.lua
+++ home/private_dot_config/nvim/lua/dcp/init.lua
@@ -1,5 +1,5 @@
--- The majority of my custom configuration lies within this module. The order
--- that these modules are loaded is important.
+-- The majority of my custom configuration lies within this module. The order that these modules are loaded is
+-- important.
 
 require("dcp.keymaps")
 require("dcp.options")
