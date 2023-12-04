400d4de8a89ed44c7554343d6af56e50816aa5fe
Dylan Pinn <dylan.pinn@rea-group.com>
2023-12-05 10:30:40 +1100

    feat(nvim): use TERMCS to set background option appropriately
    Simple plugin to set the background to the TERMCS value, if not set then
    fallback to using dark.

--- home/private_dot_config/nvim/lua/dcp/init.lua
+++ home/private_dot_config/nvim/lua/dcp/init.lua
@@ -1,6 +1,7 @@
 -- The majority of my custom configuration lies within this module. The order that these modules are loaded is
 -- important.
 
+require("dcp.set-background")
 require("dcp.keymaps")
 require("dcp.options")
 require("dcp.packages")
