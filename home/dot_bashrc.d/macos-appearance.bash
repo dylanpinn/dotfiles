96698774273fe3002cb69749f395e6e5aeb337be
Dylan Pinn <dylan.pinn@rea-group.com>
2023-12-05 10:27:13 +1100

    feat(bash): set TERMCS to match macOS system appearance
    This will allow using this value in TUI programs such as Neovim and use the
    appropriate background colour.

---
+++ home/dot_bashrc.d/macos-appearance.bash
@@ -0,0 +1,6 @@
+# Use $TERMCS to determine if macOS is using Dark or Light appearance setings.
+if [[ "$(defaults read -g AppleInterfaceStyle 2>/dev/null)" == "Dark" ]]; then
+  export TERMCS="dark"
+else
+  export TERMCS="light"
+fi
