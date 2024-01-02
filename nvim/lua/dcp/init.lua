ff0336c451fe80ef4ec034b4d453e75de82776ff
Dylan Pinn <dylan.pinn@rea-group.com>
2024-01-02 13:43:55 +1100

    style: format lua files to shorten line lengths

--- nvim/lua/dcp/init.lua
+++ nvim/lua/dcp/init.lua
@@ -1,4 +1,5 @@
--- The majority of my custom configuration lies within this module. The order that these modules are loaded is important.
+-- The majority of my custom configuration lies within this module. The order
+-- that these modules are loaded is important.
 
 --require("dcp.set-background")
 require("dcp.keymaps")
@@ -7,7 +8,8 @@ require("dcp.options")
 
 vim.cmd.colorscheme("morning")
 
--- Use ripgrep as built-in search tool, this is quicker than grep, has better interface and automatically ignores correct files/directories.
+-- Use ripgrep as built-in search tool, this is quicker than grep, has better
+-- interface and automatically ignores correct files/directories.
 if vim.fn.executable('ag') then
 	vim.o.grepprg = 'rg --vimgrep --no-heading'
 	vim.opt.grepformat:append { '%f:%l:%c:%m' }
