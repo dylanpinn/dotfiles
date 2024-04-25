8a7a906ae5ae85f206a519b2746fb0a4bb82e4ab
Dylan Pinn <me@dylanpinn.com>
2024-04-25 23:44:43 +1000

    feat(nvim): use conform to format Lua files
    This is replacing using formatprg to run the same program (stylua), this
    has extra benefits.
    
    Conform has the ability to run automatically on save, but this is
    currently using `formatexpr` to run on demand using `gq`.

--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -1,10 +1,5 @@
 -- Tabs should have 4 space width.
 vim.opt_local.tabstop = 4
 
--- Use stylua as formatter if it is available.
-if vim.fn.executable("stylua") == 1 then
-	vim.opt_local.formatprg = "stylua --stdin-filepath % --search-parent-directories -"
-end
-
 -- Use luacheck as compiler for sh files.
 vim.cmd([[compiler luacheck]])
---
+++ nvim/after/plugin/conform.lua
@@ -0,0 +1,7 @@
+local options = {
+	formatters_by_ft = {
+		lua = { "stylua" },
+	},
+}
+
+require("conform").setup(options)
