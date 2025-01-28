39babcd2bfe30782ec919ff4afa4088602bec805
Dylan Pinn <dylan.pinn@rea-group.com>
2025-01-28 15:07:49 +1100

    chore: use script shebang for shellcheck unless profile config
    These don't have a shebang as aren't scripts that are executed so these
    are explicitly told by shellcheck to use shell=sh.

--- lint/sh.sh
+++ lint/sh.sh
@@ -9,4 +9,12 @@ shellcheck --shell=sh sh/profile
 find . \
     -name "*.sh" \
     -not -path "./nvim/pack/bundle/*" \
+    -not -path "*/profile.d/*" \
+    -exec shellcheck {} +
+
+# Scripts in the `profile.d` directory are sourced by the shell, so they don't
+# have a shebang. We need to tell shellcheck to treat them as POSIX shell.
+find . \
+    -name "*/profile.d/*.sh" \
+    -not -path "./nvim/pack/bundle/*" \
     -exec shellcheck --shell=sh {} +
