7d52f522ef828f88c64d625af00060fc92b309ca
Dylan Pinn <dylan.pinn@rea-group.com>
2025-01-29 13:03:21 +1100

    Updated post-commit git hook to use extension scripts
    This follows the same convention as the previous script.

--- git/hooks/extensions/git.sh
+++ git/hooks/extensions/git.sh
@@ -8,3 +8,12 @@ git_lint_check() {
     fi
 }
 export -f git_lint_check
+
+# Label: Git Lint Analyze
+# Description: Analyze last commit.
+git_lint_analyze_commit() {
+    if _check_gem_dependencies "git-lint"; then
+        git-lint analyze --commit "$(git log --pretty=format:%H -1)"
+    fi
+}
+export -f git_lint_analyze_commit
