3f299ba38bf60999c865923a03cd74b91e25b811
Dylan Pinn <dylan.pinn@rea-group.com>
2025-10-14 12:16:20 +1100

    fix(nvim): fixup conform.nvim typescript ordering
    Nested lists have been deprecated, new config is defined here
    https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#run-the-first-available-formatter-followed-by-more-formatters

--- nvim/after/plugin/conform.lua
+++ nvim/after/plugin/conform.lua
@@ -1,11 +1,36 @@
+---@param bufnr integer
+---@param ... string
+---@return string
+local function first(bufnr, ...)
+	local conform = require("conform")
+	for i = 1, select("#", ...) do
+		local formatter = select(i, ...)
+		if conform.get_formatter_info(formatter, bufnr).available then
+			return formatter
+		end
+	end
+	return select(1, ...)
+end
+
 local options = {
 	formatters_by_ft = {
+		bash = { "shfmt" },
 		lua = { "stylua" },
-		typescript = { { "prettierd", "prettier", stop_after_first = true }, "eslint_d" },
+		typescript = function(bufnr)
+			return { first(bufnr, "prettierd", "prettier"), "eslint_d" }
+		end,
+		sh = { "shfmt" },
+	},
+	format_on_save = {
+		timeout_ms = 500,
+		lsp_fallback = true,
 	},
 }
 
 require("conform").setup(options)
+require("conform").formatters.shfmt = {
+	append_args = { "--simplify" },
+}
 
 -- This should be okay to set globally, as will default back to the LSP
 -- formatexpr if present and then to nothing.
